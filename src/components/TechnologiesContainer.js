04a7d739b5c61a53bb5cdca73093f14b48f488c2
TorugoJS <victor.devfrontend@gmail.com>
2022-08-03 21:33:12 -0300

    Portfolio

--- src/components/TechnologiesContainer.js
+++ src/components/TechnologiesContainer.js
@@ -2,51 +2,56 @@
 import { AiFillHtml5 } from 'react-icons/ai'
 import { DiCss3, DiJavascript1, DiReact, DiSass, DiBootstrap, DiGit } from 'react-icons/di'
 
+import styles from './TechnologiesContainer.module.css'
+
 
 
 
 const TechnologiesContainer = () => {
   return (
-    <section>
-      <h2>As tecnologias em que possuo experiência com projetos pessoais, são: </h2>
+    <>
+    <h2>As tecnologias em que possuo experiência com projetos pessoais, são: </h2>
+    <section className={styles.tech} >
 
-      <div>
-        <p>Tecnologia:</p>
+      <div className={styles.html}>
+        <p>HTML5:</p>
         <AiFillHtml5 />
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
       </div>
 
-      <div>
-        <p>Tecnologia:</p>
+      <div className={styles.css}>
+        <p>CSS3</p>
         <DiCss3 />
       </div>
 
-      <div>
-        <p>Tecnologia:</p>
+      <div className={styles.react}>
+        <p>React.js</p>
         <DiReact />
       </div>
 
-      <div>
-        <p>Tecnologia:</p>
+      <div className={styles.sass}>
+        <p>Sass</p>
         <DiSass />
       </div>
 
-      <div>
-        <p>Tecnologia:</p>
+      <div className={styles.boot}>
+        <p>Bootstrap</p>
         <DiBootstrap />
       </div>
-      
-      <div>
-        <p>Tecnologia:</p>
+
+      <div className={styles.git}>
+        <p>Git</p>
         <DiGit />
       </div>
 
-      <div>
-        <p>Tecnologia:</p>
+      <div className={styles.js}>
+        <p>Javascript</p>
         <DiJavascript1 />
       </div>
 
 
     </section>
+    </>
   );
 };
 
