678245dbaf64538b1ece5011ac23a3de296de422
TorugoJS <victor.devfrontend@gmail.com>
2022-08-08 19:58:39 -0300

    Portfolio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
+import Navbar from "./layout/navbar/navbar";
+import Contato from "./Sessões/Contato/Contato";
+import Home from "./Sessões/Home/Home";
+import SobreMim from "./Sessões/sobre-mim/SobreMim";
+import Tecnologias from "./Sessões/Tecnologias/Tecnologias";
+import Footer from './Sessões/Footer/Footer'
+import SwiperCarousel from "./Sessões/Carousel/SwiperCarousel";
+import BotaoTop from "./layout/Botão pra cima/BotaoTop";
 
-import './App.css';
-import MainContent from './components/MainContent';
-import Sidebar from './components/Sidebar';
-import Social from './components/Social';
 
 function App() {
   return (
-    <div className="App">
-      <div className="nav">
-        <img src="perfil.jpeg" alt='img' />
-        <p>Desenvolvedor Front-end</p>
-        <Social />
-        <h2>
-          Olá, tudo beleza? Eu sou o Victor Hugo, estudante de análise e desenvolvimento de sistemas,
-          atualmente atuo como desenvolvedor Front-end. Tenho experiências com projetos pessoais,
-          nos quais você pode conferir no meu Github!!!
-        </h2>
-      </div>
-      <Sidebar />
-      <MainContent />
-
+    
+    <div className="app">
+      <Navbar />
+      <Home />
+      <SobreMim />
+      <Tecnologias />
+      <SwiperCarousel />
+      <Contato  />
+      <Footer />
+      <BotaoTop/>
     </div>
   );
 }
