62dc992aced94b00e1d0c690dec7cf5982bc7143
TorugoJS <victor.devfrontend@gmail.com>
2022-08-15 20:16:13 -0300

    Portfolio

--- src/Sessões/Projetos/Projetos.js
+++ src/Sessões/Projetos/Projetos.js
@@ -3,23 +3,27 @@ import breve from '../imagens/breve.png'
 import './Projetos.css'
 
 const Projetos = () => {
+
   return (
 
     <div className='container'>
-
+      <h1>Projetos</h1>
       <div>
         <img alt="fotodeperfil" className='img1' src={LP} />
-        <p className='pp'> Projeto 1 Projeto 1 Projeto 1 Projeto 1 Projeto 1 Projeto 1 Projeto 1</p>
+        <h2>Landing Page</h2>
+        <p className='pp'> Landing page feita com React.js e totalmente responsiva.</p>
       </div>
 
       <div>
         <img alt="fotodeperfil" className='img1' src={breve} />
-        <p className='pp'>Projeto 1 Projeto 1 Projeto 1 Projeto 1 Projeto 1 Projeto 1 Projeto 1</p>
+        <h2>Em breve...</h2>
+        <p className='pp'>Em breve...</p>
       </div>
 
       <div>
         <img alt="fotodeperfil" className='img1' src={breve} />
-        <p className='pp'>Projeto 1 Projeto 1 Projeto 1 Projeto 1 Projeto 1 Projeto 1 Projeto 1</p>
+        <h2>Em breve...</h2>
+        <p className='pp'>Em breve...</p>
       </div>
 
     </div>
